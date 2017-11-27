5f21bec0bc51d2f29fec7b40a5780218341911d3
Jenni Pajukoski <jenni@giosg.com>
2017-11-27 22:19:33 +0200

    (Partly) fixed top right corner menu

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import IconButton from 'material-ui/IconButton';
-import Dots from 'mui-icons/cmdi/dots-vertical';
-import { MenuItem, MenuList } from 'material-ui/Menu';
+import DotsIcon from 'mui-icons/cmdi/dots-vertical';
+import Menu, { MenuItem, MenuList } from 'material-ui/Menu';
 import Grow from 'material-ui/transitions/Grow';
 import Paper from 'material-ui/Paper';
 import { Manager, Target, Popper } from 'react-popper';
 import ClickAwayListener from 'material-ui/utils/ClickAwayListener';
 import { Link } from 'react-router-dom';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 
 
 export default class Navbar extends Component {
@@ -38,34 +41,42 @@ export default class Navbar extends Component {
   render() {
     return (
       <div className="Navbar">
-        <header className="App-header">
-          <div className="App-title">Your Doctor</div>
-          <Manager>
-            <Target>
-              <IconButton className="Dots-Menu" onClick={this.handleMenuClick}>
-                <Dots className="Dots-Icon"/>
+        <AppBar position="static" className="App-header">
+          <Toolbar>
+            <Typography type="title" color="inherit" className="App-title">
+              Your Doctor
+            </Typography>
+            <div>
+              <IconButton
+                aria-owns={this.state.open ? 'menu-appbar' : null}
+                aria-haspopup="true"
+                onClick={this.handleMenuClick}
+                color="contrast"
+              >
+                <DotsIcon />
               </IconButton>
-            </Target>
-            <Popper
-              placement="bottom-start"
-              eventsEnabled={this.state.open}
-              className="Menu-list"
-            >
-              <ClickAwayListener onClickAway={this.handleRequestClose}>
-                <Grow in={this.state.open} style={{ transformOrigin: '0 0 0' }}>
-                  <Paper>
-                    <MenuList role="menu">
-                      <MenuItem onClick={this.props.handleHelpClick}>Help</MenuItem>
-                      <Link to='/login'>
-                        <MenuItem>Logout</MenuItem>
-                      </Link>
-                    </MenuList>
-                  </Paper>
-                </Grow>
-              </ClickAwayListener>
-            </Popper>
-          </Manager>
-        </header>
+              <Menu
+                id="menu-appbar"
+                anchorEl={this.state.anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={this.state.open}
+                onRequestClose={this.handleRequestClose}
+              >
+                <MenuItem onClick={this.handleRequestClose}>Help</MenuItem>
+                <Link to='/login'>
+                  <MenuItem onClick={this.handleRequestClose}>Logout</MenuItem>
+                </Link>
+              </Menu>
+            </div>
+          </Toolbar>
+        </AppBar>
       </div>
     )
   }
