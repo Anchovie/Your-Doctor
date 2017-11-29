d0f2175ae9f66381233315c76e8df8edbbfe94ec
Katja <katja.maenpaa@aalto.fi>
2017-11-29 14:07:08 +0200

    Modified information view

--- src/InformationView.js
+++ src/InformationView.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
-import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import ArrowRightIcon from 'mui-icons/cmdi/arrow-right';
 
 /* PARENTS (called from):
 * ReservationView.js
@@ -21,6 +22,31 @@ export default class InformationView extends React.Component {
     return (
         <div className="Information-view-content">
             <p> This is information view ! </p>
+
+            <div className="Information-details" >
+                <TextField 
+                    id="info-duration" 
+                    label="How long have your symptoms lasted?" 
+                    helperText="e.g. 5 days"
+                    style={{width: '25%'}}
+                />
+            </div>
+            <div>
+                <TextField
+                    id="info-details"
+                    label="Give more details if you want:"
+                    multiline
+                    style={{width:'50%'}}
+                />
+            </div>
+            <br />
+            <div>
+                <Button raised color="primary" float="right">
+                Next
+                <ArrowRightIcon className="Arrow-right-icon"/>
+                </Button>
+            </div>
+            
         </div>
     )
   }
