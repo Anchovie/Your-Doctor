9994d3b469950caab2330537cc09081f2e87f7bd
Katja <katja.maenpaa@aalto.fi>
2017-11-29 12:27:52 +0200

    Added text fields to information view

--- src/InformationView.js
+++ src/InformationView.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import IconGrid from './IconGrid';
+import TextField from 'material-ui/TextField';
+import Typography from 'material-ui/Typography';
 
 export default class InformationView extends React.Component {
 
@@ -16,7 +17,25 @@ export default class InformationView extends React.Component {
     return (
         <div className="Information-view-content">
             <p> This is information view ! </p>
-            <IconGrid />
+
+            <div className="Duration-content">
+                <Typography type="subheading" component="h2">
+                    Duration
+                </Typography>
+                <Typography type="body1" component="p">
+                    How many days have your symptoms lasted?
+                </Typography>
+                <TextField />
+            </div>
+            <div className="Free-text-container">
+                <Typography type="subheading" component="h2">
+                    Details
+                </Typography>
+                <Typography type="body1" component="p">
+                    You can give more details here:
+                </Typography>
+                <TextField multiline />
+            </div>
         </div>
     )
   }
