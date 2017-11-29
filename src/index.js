0a17538ebb9605428e2c1a21896fda1788e93cf1
Jenni Pajukoski <jenni@giosg.com>
2017-11-29 19:12:14 +0200

    Added transition to top bar height

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const Root = () => (
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
+  );
+
+ReactDOM.render(<Root />, document.getElementById('root'));
