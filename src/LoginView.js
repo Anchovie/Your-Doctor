71b972bf544a49654059ba9c6a4fcd4756686c48
oukka <annika.oukka@gmail.com>
2017-12-04 15:00:54 +0200

    More changes to login view.

--- src/LoginView.js
+++ src/LoginView.js
@@ -6,6 +6,7 @@ import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import HeartIcon from 'mui-icons/fontawesome/heart';
 import DoctorIcon from 'mui-icons/fontawesome/user-md';
 import ListIcon from 'mui-icons/fontawesome/th-list';
+import logoImage from './img/logo.png';
 
 
 /* PARENTS (called from):
@@ -16,37 +17,44 @@ export default class LoginView extends React.Component {
 
   render() {
     return (
-      <Grid className="LoginPage" container spacing={24} justify="center">
-        <Grid item xs={8} sm={6} md={4} justify="center">
-          <List>
-            <ListItem>
-              <ListItemIcon>
-                <ListIcon className="loginIcon" />
-              </ListItemIcon>
-              <ListItemText class="LoginTexts" inset primary="Reserve appointments easily" />
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <DoctorIcon className="loginIcon" />
-              </ListItemIcon>
-              <ListItemText class="LoginTexts" inset primary="Meet your doctor online" />
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <HeartIcon className="loginIcon" />
-              </ListItemIcon>
-              <ListItemText class="LoginTexts" inset primary="Enjoy and feel healthy!" />
-            </ListItem>
-          </List>
-          <div className="LoginView">
-            <Link to='/'>
-              <Button raised aria-label="Login" color="accent">
-                Login
-              </Button>
-            </Link>
-          </div>
+      <div className="LoginPage">
+        <Grid className="LogoContainer" container spacing={24} justify="center">
+          <Grid item>
+            <img className="Logo-Image" src={logoImage} alt="" />
+          </Grid>
         </Grid>
-      </Grid>
+        <Grid container spacing={24} justify="center">
+          <Grid item xs={8} sm={6} md={4}>
+            <List class="LoginList">
+              <ListItem>
+                <ListItemIcon>
+                  <ListIcon className="loginIcon" />
+                </ListItemIcon>
+                <ListItemText class="LoginTexts" inset primary="Reserve appointments easily" />
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <DoctorIcon className="loginIcon" />
+                </ListItemIcon>
+                <ListItemText class="LoginTexts" inset primary="Meet your doctor online" />
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <HeartIcon className="loginIcon" />
+                </ListItemIcon>
+                <ListItemText class="LoginTexts" inset primary="Enjoy and feel healthy!" />
+              </ListItem>
+            </List>
+            <div className="LoginButton">
+              <Link to='/'>
+                <Button raised aria-label="Login" color="accent">
+                  Login
+                </Button>
+              </Link>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
     );
   }
 }
