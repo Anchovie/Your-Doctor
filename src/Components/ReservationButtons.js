f71a3ef90596a9e1ddb9c19174ba9c6355684d3f
Jenni Pajukoski <jenni@giosg.com>
2017-12-04 11:51:53 +0200

    Fixed reservation action buttons position

--- src/Components/ReservationButtons.js
+++ src/Components/ReservationButtons.js
@@ -14,16 +14,16 @@ function ReservationButtons(props) {
     <div>
       {(props.currentStep === 1 || props.currentStep === 2) &&
         <div className="Button-bar">
-          <Button dense color="primary" onClick={props.handleBackClick}>
+          <Button dense color="primary" onClick={props.handleBackClick} className="New-reservation-action-button">
             Previous
           </Button>
           {props.isNextDisabled ? (
-            <Button disabled raised color="primary">
+            <Button disabled raised color="primary" className="New-reservation-action-button">
               Next
               <ArrowRightIcon/>
             </Button>
           ) : (
-            <Button className="PulseButton" raised color="primary" onClick={props.handleNextClick}>
+            <Button className="PulseButton New-reservation-action-button" raised color="primary" onClick={props.handleNextClick}>
               Next
               <ArrowRightIcon className="Arrow-right-icon"/>
             </Button>
@@ -32,17 +32,17 @@ function ReservationButtons(props) {
       }
       {props.currentStep === 3 &&
         <div className="Button-bar">
-          <Button dense color="primary" onClick={props.handleBackClick}>
+          <Button dense color="primary" onClick={props.handleBackClick} className="New-reservation-action-button">
             Previous
           </Button>
         </div>
       }
       {props.currentStep === 4 &&
         <div className="Button-bar">
-          <Button dense color="primary" onClick={props.handleBackClick}>
+          <Button dense color="primary" onClick={props.handleBackClick} className="New-reservation-action-button">
             Previous
           </Button>
-          <div className="RightSideButtons">
+          <div className="RightSideButtons" className="New-reservation-action-button">
             <Link to='/'>
               <Button className="cancelButton" dense color="default" onClick={props.handleBackClick}>
               Cancel
