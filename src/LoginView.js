6122decb51ef0103ebb8bf9d819f8ddf170e2d53
Katja <maenpaa.katja@gmail.com>
2017-12-08 17:35:16 +0200

    Login layout

--- src/LoginView.js
+++ src/LoginView.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Button from 'material-ui/Button';
 import { Link } from 'react-router-dom';
 import Grid from 'material-ui/Grid';
-import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import HeartIcon from 'mui-icons/fontawesome/heart';
 import DoctorIcon from 'mui-icons/fontawesome/user-md';
 import ListIcon from 'mui-icons/fontawesome/th-list';
 import logoImage from './img/logo.png';
+import Typography from 'material-ui/Typography';
 
 
 /* PARENTS (called from):
@@ -18,33 +18,34 @@ export default class LoginView extends React.Component {
   render() {
     return (
       <div className="LoginPage">
-        <Grid className="LogoContainer" container spacing={24} justify="center">
-          <Grid item>
-            <img className="Logo-Image" src={logoImage} alt="" />
+        <Grid className="LoginContainer" container spacing={24} justify="center">
+          <Grid item className="HeaderContainer" justify="center" xs={12} sm={12} md={12}>
+              <img className="Logo-Image" src={logoImage} alt="" />
+              <Typography type="display1" className="LogoName">
+                Your Doctor
+              </Typography>
           </Grid>
-        </Grid>
-        <Grid container spacing={24} justify="center">
-          <Grid item xs={8} sm={6} md={4}>
-            <List className="LoginList">
-              <ListItem>
-                <ListItemIcon>
-                  <ListIcon className="loginIcon" />
-                </ListItemIcon>
-                <ListItemText className="LoginTexts" inset primary="Reserve appointments easily" />
-              </ListItem>
-              <ListItem>
-                <ListItemIcon>
-                  <DoctorIcon className="loginIcon" />
-                </ListItemIcon>
-                <ListItemText className="LoginTexts" inset primary="Meet your doctor online" />
-              </ListItem>
-              <ListItem>
-                <ListItemIcon>
-                  <HeartIcon className="loginIcon" />
-                </ListItemIcon>
-                <ListItemText className="LoginTexts" inset primary="Enjoy and feel healthy!" />
-              </ListItem>
-            </List>
+          <Grid item className="ListContainer" justify="center" xs={8} sm={5} md={3}>
+            <div className="LoginList">
+              <ListIcon className="loginIcon"/>
+              <Typography type="h3" className="LoginTexts">
+                Reserve appointments easily
+              </Typography>
+            </div>
+            <div className="LoginList">
+              <DoctorIcon className="loginIcon"/>
+              <Typography type="h3" className="LoginTexts">
+                Meet your doctor online
+              </Typography>
+            </div>
+            <div className="LoginList">
+              <HeartIcon className="loginIcon"/>
+              <Typography type="h3" className="LoginTexts">
+                Enjoy and feel healthy!
+              </Typography>
+            </div>
+          </Grid>
+          <Grid item className="ButtonContainer" spacing={24} justify="center" xs={12} sm={12} md={12}>
             <div className="LoginButton">
               <Link to='/'>
                 <Button raised aria-label="Login" color="accent">
@@ -52,7 +53,7 @@ export default class LoginView extends React.Component {
                 </Button>
               </Link>
             </div>
-          </Grid>
+            </Grid>
         </Grid>
       </div>
     );
